2e595285ca6da6b9c1172aab00a269086827388b
Arun <arunmahi921@gmail.com>
2022-05-18 13:58:37 +0530

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const button = document.getElementById('nav-button');
+const header=document.getElementById('header');
+
+button.addEventListener('click',function(e){
+  e.preventDefault();
+  
+
+} )
